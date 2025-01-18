b75a8b5c602ed7b26f4794df3393cda85a7fd5a8
Mykola Dotsenko <mykola.dotsenko@nidos.fi>
2025-01-19 00:39:11 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
+import { CurrencyPipe, DatePipe, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [DatePipe, UpperCasePipe, CurrencyPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-movies';
+  movies = [{
+    title: 'The Avengers',
+    releaseDate: new Date(),
+    price: 19.99,
+  },
+{
+  title: 'The Spider-Man',
+  releaseDate: new Date('2016-05-03'),
+  price: 29.99,
+}
+]
+
+  duplicateNumber(value: number) {
+    return value * 2;
+  }
 }
