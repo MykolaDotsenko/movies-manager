35bb3e9f4a81c58ee2070d1a5e18861c026d37ee
Mykola Dotsenko <docnikolaj1990@gmail.com>
2025-02-06 01:56:28 +0200

    Navigating with JavaScript

---
+++ src/app/actors/create-actors/create-actors.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-actors',
+  imports: [],
+  templateUrl: './create-actors.component.html',
+  styleUrl: './create-actors.component.css'
+})
+export class CreateActorsComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,24 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { IndexGenresComponent } from './genres/index-genres/index-genres.component';
+import { CreateGenreComponent } from './genres/create-genre/create-genre.component';
+import { IndexActorsComponent } from './actors/index-actors/index-actors.component';
+import { CreateActorsComponent } from './actors/create-actors/create-actors.component';
+import { IndexTheatersComponent } from './theaters/index-theaters/index-theaters.component';
+import { CreateTheaterComponent } from './theaters/create-theater/create-theater.component';
+import { CreateMovieComponent } from './movies/create-movie/create-movie.component';
 
 export const routes: Routes = [
   {path: '', component: LandingPageComponent},
-  {path: 'genres', component: IndexGenresComponent}
+  {path: 'genres', component: IndexGenresComponent},
+  {path: 'genres/create', component: CreateGenreComponent},
+
+  {path: 'actors', component: IndexActorsComponent},
+  {path: 'actors/create', component: CreateActorsComponent},
+
+  {path: 'theaters', component: IndexTheatersComponent},
+  {path: 'theaters/create', component: CreateTheaterComponent},
+
+  {path: 'movies/create', component: CreateMovieComponent},
+
 ];
