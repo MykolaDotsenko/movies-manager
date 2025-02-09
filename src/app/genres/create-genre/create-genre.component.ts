5c5a2952fd9dda343b0d3e386f3ca4d90a235d26
Mykola Dotsenko <docnikolaj1990@gmail.com>
2025-02-09 23:58:57 +0200

    form builder

--- src/app/genres/create-genre/create-genre.component.ts
+++ src/app/genres/create-genre/create-genre.component.ts
@@ -1,16 +1,22 @@
 import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-genre',
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, ReactiveFormsModule],
   templateUrl: './create-genre.component.html',
   styleUrl: './create-genre.component.css'
 })
 export class CreateGenreComponent {
 
 router = inject(Router)
+private formBuilder = inject(FormBuilder);
+
+form = this.formBuilder.group({
+  name: ['']
+});
 
 saveChanges() {
 // ..save changes
