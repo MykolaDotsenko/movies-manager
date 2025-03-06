fbc529ef12b2c505d8d3fdd8676a2aadb87202d3
Mykola Dotsenko <docnikolaj1990@gmail.com>
2025-03-06 17:49:44 +0200

    Movie's Form

--- src/app/movies/edit-movie/edit-movie.component.ts
+++ src/app/movies/edit-movie/edit-movie.component.ts
@@ -1,13 +1,26 @@
 import { Component, Input, numberAttribute } from '@angular/core';
+import { MovieCreationDTO, MovieDTO } from '../movies.models';
 
 @Component({
   selector: 'app-edit-movie',
   imports: [],
   templateUrl: './edit-movie.component.html',
-  styleUrl: './edit-movie.component.css'
+  styleUrl: './edit-movie.component.css',
 })
 export class EditMovieComponent {
+  @Input({ transform: numberAttribute })
+  id!: number;
 
-@Input({transform: numberAttribute})
-id!: number;
+  model: MovieDTO = {
+    id: 1,
+    title: 'Spider-Man: Across the Spider-Verse',
+    releaseDate: new Date('2018-07-22'),
+    trailer: 'abcd',
+    poster:
+      'https://m.media-amazon.com/images/M/MV5BMjMwNDkxMTgzOF5BMl5BanBnXkFtZTcwNTU5OTUyNw@@._V1_FMjpg_UX1000_.jpg',
+  };
+
+  saveChanges(movie: MovieCreationDTO) {
+    console.log('Editing the movie', movie);
+  }
 }
