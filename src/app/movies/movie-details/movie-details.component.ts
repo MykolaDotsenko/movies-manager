befc7805df2eeac3e6fe19fb368fa4baf4727c44
Mykola Dotsenko <docnikolaj1990@gmail.com>
2025-04-11 23:25:22 +0300

    Rating's Component

--- src/app/movies/movie-details/movie-details.component.ts
+++ src/app/movies/movie-details/movie-details.component.ts
@@ -7,16 +7,19 @@ import {
 } from '@angular/core';
 import { MoviesService } from '../movies.service';
 import { MovieDTO } from '../movies.models';
-import { LoadingComponent } from "../../shared/components/loading/loading.component";
+import { LoadingComponent } from '../../shared/components/loading/loading.component';
 import { MatChipsModule } from '@angular/material/chips';
 import { RouterLink } from '@angular/router';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { Coordinate } from '../../shared/components/map/Coordinate.module';
-import { MapComponent } from "../../shared/components/map/map.component";
+import { MapComponent } from '../../shared/components/map/map.component';
+import { RatingService } from '../../rating/rating.service';
+import Swal from 'sweetalert2';
+import { RatingComponent } from "../../shared/components/rating/rating.component";
 
 @Component({
   selector: 'app-movie-details',
-  imports: [LoadingComponent, MatChipsModule, RouterLink, MapComponent],
+  imports: [LoadingComponent, MatChipsModule, RouterLink, MapComponent, RatingComponent],
   templateUrl: './movie-details.component.html',
   styleUrl: './movie-details.component.css',
 })
@@ -24,37 +27,50 @@ export class MovieDetailsComponent implements OnInit {
   @Input({ transform: numberAttribute })
   id!: number;
   movie!: MovieDTO;
-trailerURL!: SafeResourceUrl;
+  trailerURL!: SafeResourceUrl;
   sanitizer = inject(DomSanitizer);
 
   moviesService = inject(MoviesService);
+  ratingServise = inject(RatingService)
   coordinates: Coordinate[] = [];
 
   ngOnInit(): void {
-  this.moviesService.getById(this.id).subscribe(movie => {
-    this.movie = movie;
-    movie.releaseDate = new Date(movie.releaseDate);
-    this.trailerURL = this.transformYoutubeURLToEmbed(this.movie.trailer);
-    if(movie.theaters){
-      this.coordinates = movie.theaters.map(theater => {
-        return {latitude: theater.latitude, longitude: theater.longitude, text: theater.name}
-      });
+    this.moviesService.getById(this.id).subscribe((movie) => {
+      this.movie = movie;
+      movie.releaseDate = new Date(movie.releaseDate);
+      this.trailerURL = this.transformYoutubeURLToEmbed(this.movie.trailer);
+      if (movie.theaters) {
+        this.coordinates = movie.theaters.map((theater) => {
+          return {
+            latitude: theater.latitude,
+            longitude: theater.longitude,
+            text: theater.name,
+          };
+        });
+      }
+    });
+  }
+
+  transformYoutubeURLToEmbed(url: string): SafeResourceUrl | string {
+    if (!url) {
+      return '';
     }
 
-  });
+    let videoId = url.split('v=')[1];
+    let ampersandPosition = videoId.indexOf('&');
+    if (ampersandPosition !== -1) {
+      videoId = videoId.substring(0, ampersandPosition);
+    }
+
+    return this.sanitizer.bypassSecurityTrustResourceUrl(
+      `https://www.youtube.com/embed/${videoId}`
+    );
   }
 
-  transformYoutubeURLToEmbed(url: string) : SafeResourceUrl | string {
-    if(!url){ return ''};
 
-
-  let videoId = url.split('v=')[1];
-  let ampersandPosition = videoId.indexOf('&');
-  if(ampersandPosition !== -1){
-    videoId = videoId.substring(0, ampersandPosition);
+  rate(rate: number) {
+    this.ratingServise.rate(this.id, rate).subscribe(() => {
+      Swal.fire('Successful', 'Your rate has been received', 'success');
+    });
   }
-
-  return this.sanitizer.bypassSecurityTrustResourceUrl(`https://www.youtube.com/embed/${videoId}`);
 }
-}
-
